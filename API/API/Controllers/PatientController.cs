0099fddb309d0357953f564e2f34100f67f78c89
hongnhung <hongnhung10101998@gmail.com>
2022-11-28 16:15:57 +0700

    Add API

---
+++ API/API/Controllers/PatientController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    [RoutePrefix("api/patient")]
+    public class PatientController : ApiController
+    {
+        DBContext DB = new DBContext();
+        [Route("Studentdetails")]
+        [HttpGet]
+        public object Studentdetails()
+        {
+
+            var a = DB.Patients.ToList();
+            return a;
+        }
+
+    }
+}
---
+++ API/API/Models/Patient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Models
+{
+    public class Patient
+    {
+        public int PatientID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string Phone { get; set; }
+        public string AddressPatient { get; set; }
+        public string City { get; set; }
+        public string Sate { get; set; }
+        public string Zipcode { get; set; }
+    }
+    public class Response
+    {
+        public string Status { get; set; }
+        public string Message { get; set; }
+    }
+}
